314a32802ac1273daccdbc97875539beafe42286
CEYeYuan <eceye.yuan@mail.utoronto.ca>
2016-02-22 17:42:58 -0500

    lease enter the commit message for your changes. Lines starting

--- interactive/app/src/main/java/com/example/ye/interactive/MainActivity.java
+++ interactive/app/src/main/java/com/example/ye/interactive/MainActivity.java
@@ -8,18 +8,60 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
 import android.widget.RelativeLayout;
 import android.widget.Button;
+import android.graphics.Color;
+import android.widget.EditText;
+import android.content.res.Resources;
+import android.util.TypedValue;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //layout
         RelativeLayout main_layout= new RelativeLayout(this);
-        Button redButton = new Button(this);
+        main_layout.setBackgroundColor(Color.GREEN);
 
-        main_layout.addView(redButton);
+
+        //button
+        Button redButton = new Button(this);
+        redButton.setId(1);
+        redButton.setText("Log In");
+        redButton.setBackgroundColor(Color.RED);
+        //make a container; the same size of the input
+        RelativeLayout.LayoutParams buttonDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+        buttonDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        buttonDetails.addRule(RelativeLayout.CENTER_VERTICAL);
+
+        Resources r =getResources();
+        //DIP device independent pixels
+        int px = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,200,
+                r.getDisplayMetrics());
+
+
+        //username
+        EditText username = new EditText(this);
+        username.setWidth(px);
+        username.setId(2);
+        RelativeLayout.LayoutParams usernameDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+        //give rules to position widgets
+        usernameDetails.addRule(RelativeLayout.ABOVE,1);//1 is the id for the red button
+        usernameDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        usernameDetails.setMargins(0,0,0,50);
+
+
+        //add widgets using the rules to the view
+        main_layout.addView(redButton,buttonDetails);
+        main_layout.addView(username,usernameDetails);
         setContentView(main_layout);
 
     }
